15efb2d79eb439134e47bfd9c98bb7112d298d43
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2023-02-24 17:26:43 +0000

    updated addGuildAdmin functions

--- events/addGuildAdmin.js
+++ events/addGuildAdmin.js
@@ -8,36 +8,34 @@ var cron = require('node-cron'); //https://github.com/node-cron/node-cron
  	name: 'ready',
  	async execute(client) {
 
+		function gtaRemove() {
 		fs.readFile('./GTADataBase.txt', 'utf8', async function (err, data) {
  			if (err) {console.log(`Error: ${err}`)} //If an error, console.log
 				const GuildIDs = client.guilds.cache.map(guild => guild.id);
-				const guildIdString = GuildIDs.join();
-					//console.log(`guildIdString: ${guildIdString}`);
-
-				const gtaGuildIDs = [];
-				for (i=0;i<=GuildIDs.length;i++) {
-					if (data.includes(GuildIDs[i]) ) {
-						gtaGuildIDs.push(GuildIDs[i]);
-					}
-				}
-				//console.log(`gtaGuildIDs: ${gtaGuildIDs}`);
-				//console.log(`gtaGuildIDsString: ${gtaGuildIDs.toString()}`)
-
-				//DO NOT UNCOMMENT
-				//DO NOT UNCOMMENT!!!   //gtaGuildIDs.shift();   // DO NOT UNCOMMENT!!! testbench to make sure it still works
-		    //DO NOT UNCOMMENT                                 
+				const channelIDs01 = data.split("channel:");
+				const gtaGuildIDs01 = data.split("guild:");
+				const guildIdString = GuildIDs.toString();
+					//console.log(`guildIdString: ${guildIdString}`);                              
 			
 				let newData = data;
-				for (i=0;i<=GuildIDs.length - 1;i++) {
-					if (!gtaGuildIDs.toString().includes(GuildIDs[i])) { //if a guild id in GTADataBase.txt is not a guild that the bot is in, delete subscription
-							const find = `guild:${GuildIDs[i]} - channel:`;
-							const replace = `guild:undefined${GuildIDs[i]} - channel:undefined`;
+				for (i = 2; i <= gtaGuildIDs01.length - 1; i++) {
+					const channelID01 = channelIDs01[i].split(" ");
+					const channelID = channelID01[0];
+					const guildID01 = gtaGuildIDs01[i].split(" ");
+					const guildID = guildID01[0];
+					// console.log(`channelID at ${i}: ${channelID}`);
+					// console.log(`guildID at ${i}: ${guildID}`);
+					if (!guildIdString.includes(guildID))  {//if a guild id in GTADataBase.txt is not a guild that the bot is in, delete subscription
+							const find = `\nguild:${guildID} - channel:${channelID} - rdo_gta:gtaStartMenu - `;
+							const replace = "";
 							newData = newData.replace(new RegExp(find, 'g'), replace);
+							console.log(`The bot was removed from ${guildID} with GTA posts still subscribed to ${channelID} channel.`);
 				}//end for loop
-				//console.log(`newData: ${newData}`);
+				// console.log(`data at ${i}: ${data}`);
+				// console.log(`newData: ${newData}`);
+				// console.log(`data !== newData? ${data !== newData}`);
 
-					if (!data === newData) { //the bot was kicked from a guild that still had gta channels subscribed
-						console.log(`The bot was removed from a guild with GTA posts still subscribed`)
+					if (data !== newData) { //the bot was kicked from a guild that still had gta channels subscribed
 				    //Replaces the rolesDataBase.txt file with Admin permission for the guild
 				    fs.writeFile(`./GTADataBase.txt`,`${newData}`, err => {
 							if (err) {
@@ -49,49 +47,100 @@ var cron = require('node-cron'); //https://github.com/node-cron/node-cron
 				}
 			
 			}); //END FS:READFILE gtaDataBase.txt
-			
+		} // end gtaRemove()
+		gtaRemove();
+		setInterval(gtaRemove, 864e5); //every 24 hours
 		
-
+		function rdoRemove() {
 		fs.readFile('./RDODataBase.txt', 'utf8', async function (err, data) {
  			if (err) {console.log(`Error: ${err}`)} //If an error, console.log
 				const GuildIDs = client.guilds.cache.map(guild => guild.id);
-				const rdoGuildIDs = [];
-				const guildIdString = GuildIDs.join();
-
-				for (i=0;i<=GuildIDs.length;i++) {
-					if (data.includes(GuildIDs[i]) ) {
-						rdoGuildIDs.push(GuildIDs[i]);
-					}
-				}
-
-				//DO NOT UNCOMMENT
-				//DO NOT UNCOMMENT!!!   //rdoGuildIDs.shift();   // DO NOT UNCOMMENT!!! testbench to make sure it still works
-		    //DO NOT UNCOMMENT                                 
+				const channelIDs01 = data.split("channel:");
+				const rdoGuildIDs01 = data.split("guild:");
+				const guildIdString = GuildIDs.toString();
+					//console.log(`guildIdString: ${guildIdString}`);                              
 			
 				let newData = data;
-				for (i=0;i<=GuildIDs.length - 1;i++) {
-					if (!guildIdString.includes(rdoGuildIDs[i])) { //if a guild id in GTADataBase.txt is not a guild that the bot is in, delete subscription	
-
-							const find = `guild:${GuildIDs[i]} - channel:`;
-							const replace = `guild:undefined${GuildIDs[i]} - channel:undefined`;
+				for (i = 2; i <= rdoGuildIDs01.length - 1; i++) {
+					const channelID01 = channelIDs01[i].split(" ");
+					const channelID = channelID01[0];
+					const guildID01 = rdoGuildIDs01[i].split(" ");
+					const guildID = guildID01[0];
+					// console.log(`channelID at ${i}: ${channelID}`);
+					// console.log(`guildID at ${i}: ${guildID}`);
+					if (!guildIdString.includes(guildID))  {//if a guild id in RDODataBase.txt is not a guild that the bot is in, delete subscription
+							const find = `\nguild:${guildID} - channel:${channelID} - rdo_gta:rdoStartMenu - `;
+							const replace = "";
 							newData = newData.replace(new RegExp(find, 'g'), replace);
-					}
+							console.log(`The bot was removed from ${guildID} with RDO posts still subscribed to ${channelID} channel.`);
 				}//end for loop
-				//console.log(`newData: ${newData}`);
+				// console.log(`data at ${i}: ${data}`);
+				// console.log(`newData: ${newData}`);
+				// console.log(`data !== newData? ${data !== newData}`);
 
-				if (!data === newData) {
-					console.log(`Somone removed the bot with RDO posts still subscribed.`);		
-		    //Replaces the rolesDataBase.txt file with Admin permission for the guild
-			    fs.writeFile(`./RDODataBase.txt`,`${newData}`, err => {
-			        if (err) {
-			            console.error(err)
-			            return
-			            }					
-			    }); //end fs.writeFile to change the admin privileges				
+					if (data !== newData) { //the bot was kicked from a guild that still had rdo channels subscribed
+				    //Replaces the rolesDataBase.txt file with Admin permission for the guild
+				    fs.writeFile(`./RDODataBase.txt`,`${newData}`, err => {
+							if (err) {
+									console.error(err)
+									return
+									}					
+				    }); //end fs.writeFile to change the admin privileges		
+					}
 				}
-			}); //END FS:READFILE gtaDataBase.txt
 			
-		
+			}); //END FS:READFILE rdoDataBase.txt
+		} // end rdoRemove()
+		rdoRemove();
+		setInterval(rdoRemove, 864e5); //every 24 hours		
+
+
+		function roleRemove() {
+		fs.readFile('./rolesDataBase.txt', 'utf8', async function (err, data) {
+ 			if (err) {console.log(`Error: ${err}`)} //If an error, console.log
+				const GuildIDs = client.guilds.cache.map(guild => guild.id);
+				const roleGuildIDs01 = data.split("guild:");
+				const adminIDs01 = data.split("admin:");
+				const roleIDs01 = data.split("role:");
+				const guildIdString = GuildIDs.toString();
+					//console.log(`guildIdString: ${guildIdString}`);                              
+			
+				let newData = data;
+				for (i = 2; i <= roleGuildIDs01.length - 1; i++) {
+					const guildID01 = roleGuildIDs01[i].split(" ");
+					const guildID = guildID01[0];
+					const adminID01 = adminIDs01[i].split(" ");
+					const adminID = adminID01[0];	
+					const roleID01 = roleIDs01[i].split(" ");
+					const roleID = roleID01[0];		
+					// console.log(`guildID at ${i}: ${guildID}`);
+					// console.log(`adminID at ${i}: ${adminID}`);
+					// console.log(`roleID at ${i}: ${roleID}`);
+					if (!guildIdString.includes(guildID))  {//if a guild id in rolesDataBase.txt is not a guild that the bot is in, delete subscription
+							const find = `\nguild:${guildID} - admin:${adminID} - role:${roleID} - `;
+							const replace = "";
+							newData = newData.replace(new RegExp(find, 'g'), replace);
+							console.log(`The bot was removed from ${guildID} - admin:${adminID} - role:${roleID} still subscribed.`);
+				}//end for loop
+				// console.log(`data at ${i}: ${data}`);
+				// console.log(`newData: ${newData}`);
+				// console.log(`data !== newData? ${data !== newData}`);
+
+					if (data !== newData) { //the bot was kicked from a guild that still had a role subscribed
+				    //Replaces the rolesDataBase.txt file with Admin permission for the guild
+				    fs.writeFile(`./rolesDataBase.txt`,`${newData}`, err => {
+							if (err) {
+									console.error(err)
+									return
+									}					
+				    }); //end fs.writeFile to change the admin privileges		
+					}
+				}
+			
+			}); //END FS:READFILE rolesDataBase.txt		
+		}	//end of roleRemove()
+			roleRemove();
+			setInterval(roleRemove, 864e5); //every 24 hours		
 
  }
  }
