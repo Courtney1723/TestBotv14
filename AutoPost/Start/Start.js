1aa6a9e1589327add55a6a0a7ac777176e4862c2
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2023-04-24 19:12:24 +0000

    Removed configure options from the start button

--- AutoPost/Start/Start.js
+++ AutoPost/Start/Start.js
@@ -15,37 +15,6 @@ module.exports = {
 			//console.log(`start interaction.user.id === buttonUserID? ${interaction.user.id === buttonUserID}`);
 			//console.log(`start interaction.user.id: ${interaction.user.id} && buttonUserID: ${buttonUserID}`);
 
-		let guildRoleIds = [];
-		fs.readFile('./rolesDataBase.txt', 'utf8', async function (err, data) {
-		    if (err) {console.log(`Error: ${err}`)} //If an error, console.log
-		
-					interaction.guild.roles.cache.forEach(role => {
-							if (data.includes(role.id)) {
-								guildRoleIds.push(role.id);
-							}
-					});
-			guildRoleIds.splice(guildRoleIds.length - 1); //.splice(guildRoleIds.length - 1) removes the @everyone role
-				//console.log(`guildRoleIds: ${guildRoleIds}`);
-
-			function AdminRequired() {
-				let AdminRequiredBoolean = data.split(`guild:${interaction.guild.id} - admin:`);
-				if (AdminRequiredBoolean[1] === undefined) {
-					 	fs.appendFile(`./rolesDataBase.txt`,`guild:${interaction.guild.id} - admin:yes - role:undefined - \n`, err => {
- 							if (err) {
- 								console.error(err)
- 								return
- 							}					
- 						}); //end fs.appendFile	
-				}
-				else if (AdminRequiredBoolean[1].startsWith(`yes`)) {
-					return "AdminRequiredYes";
-				}
-				else {
-					return "AdminRequiredNo";
-				}
-			}		
-				//console.log(`AdminRequired(): ${AdminRequired()}`)
-
 //--BEGIN TRANSLATIONS--//
 			fs.readFile('./LANGDataBase.txt', 'utf8', async function (err, data) {
 			  if (err) {console.log(`Error: ${err}`)} 
@@ -300,52 +269,15 @@ Click **\'RDO\'** to set up Red Dead Online Auto Posts for **the first Tuesday o
 //begin checking for permissions
 		await interaction.deferUpdate();
 		//console.log(`AdminRequired(): ${AdminRequired()}`)
-		if (interaction.user.id != buttonUserID) {
+		if (!interaction.user.id === buttonUserID) {
 			await interaction.followUp({ content: `${notYourButtonString()}`, ephemeral: true });
 		}	
-		else if (AdminRequired() === undefined) {
-				await interaction.followUp({ content: `${firstCommandString()}`, ephemeral: true });
-		}
-		else if (AdminRequired() === "AdminRequiredYes") { //if admin permissions are required
-			if ( (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) && (interaction.user.id === buttonUserID) ) {
-					await interaction.editReply({ embeds: [startEmbed], components: [startButtons] }).catch(err => console.log(`startEmbed Error: ${err}`));	
-			} 
-			else if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
-					await interaction.followUp({content: `${missingPermissions()}`, ephemeral: true});
-			}
-				
-			else if (!interaction.user.id === buttonUserID)  {
-					await interaction.followUp({ content: `${notYourButtonString()}`, ephemeral: true });	
-			}
-		}
-			
-		else if (AdminRequired() === "AdminRequiredNo") { //if admin permissions are NOT required
-
-				//console.log(`guildRoleIds.length: ${guildRoleIds.length}`)
-				let hasARole = 0;
-				for (a=0;a<=guildRoleIds.length - 1;a++) { //iterates through each role
-					//console.log(`guildRoleIds at ${i}: ${guildRoleIds[i]}`);
-					if (interaction.member.roles.cache.has(guildRoleIds[a])) {
-						hasARole += 1;
-					}
-				} //end loop to check for hasARole
-					//console.log(`hasARole: ${hasARole} && required roles:${guildRoleIds.length}`)
-				if ( (guildRoleIds.length === 0) && (interaction.user.id === buttonUserID) ) { //no role required
-					await interaction.editReply({ embeds: [startEmbed], components: [startButtons] }).catch(err => console.log(`startButtons Error: ${err.stack}`));
-				}
-					
-				else if ( (hasARole >= 1) && (interaction.user.id === buttonUserID) ) { //if the user has at least one role listed
-						await interaction.editReply({ embeds: [startEmbed], components: [startButtons] }).catch(err => console.log(`startButtons Error: ${err.stack}`));
-				}
-					
-				else if ( (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) && (interaction.user.id === buttonUserID) ) { //If the user is an administrator
-						await interaction.editReply({ embeds: [startEmbed], components: [startButtons] }).catch(err => {console.log(`startButtons Error: ${err.stack}`); process.kill(1);});
-				}		
-				else if (hasARole <= 0) { //if the user does not have a listed role and is not an administrator
-					await interaction.followUp({content: `${missingPermissions()}`, ephemeral: true});
-				}											
-		}
-			
+		else if ( (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) && (interaction.user.id === buttonUserID) ) {
+				await interaction.editReply({ embeds: [startEmbed], components: [startButtons] }).catch(err => console.log(`startEmbed Error: ${err}`));	
+		} 
+		else if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+				await interaction.followUp({content: `${missingPermissions()}`, ephemeral: true});
+		}		
 		else {
 			await interaction.followUp({ content: `${errorString()}`, ephemeral: true });
 		} //end checking for permissions	
@@ -386,7 +318,7 @@ Click **\'RDO\'** to set up Red Dead Online Auto Posts for **the first Tuesday o
 				}, (60000 * 5))						
 
 				}});// end fs:readFile for LANGData.txt
-		}); //end fs:readFile			
+			
 	
 	} //end if start
 	},
